ff1f813c7e7945b688ecc6a0369ed8f3526ae6ee
George Miller <git@duffleman.co.uk>
2021-01-15 12:38:02 +0000

    add "slicecontains"

---
+++ lib/slicecontains/slicecontains.go
@@ -0,0 +1,31 @@
+package slicecontains
+
+func String(slice []string, val string) bool {
+	for _, v := range slice {
+		if v == val {
+			return true
+		}
+	}
+
+	return false
+}
+
+// SameStrings checks that all of the strings exist in a and b.
+func SameStrings(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	keys := map[string]struct{}{}
+	for _, v := range a {
+		keys[v] = struct{}{}
+	}
+
+	for _, v := range b {
+		if _, ok := keys[v]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ lib/slicecontains/slicescontains_test.go
@@ -0,0 +1,41 @@
+package slicecontains_test
+
+import (
+	"testing"
+
+	"github.com/cuvva/cuvva-public-go/lib/slicecontains"
+)
+
+func TestEmpty(t *testing.T) {
+	var a []string
+	var b []string
+
+	if !slicecontains.SameStrings(a, b) {
+		t.Error("slices should be equal")
+	}
+
+	a = []string{}
+	b = []string{}
+
+	if !slicecontains.SameStrings(a, b) {
+		t.Error("slices should be equal")
+	}
+}
+
+func TestSameStringsMatch(t *testing.T) {
+	a := []string{"a", "b"}
+	b := []string{"a", "c"}
+
+	if slicecontains.SameStrings(a, b) {
+		t.Error("slices should not be equal")
+	}
+}
+
+func TestSameStringsSubset(t *testing.T) {
+	a := []string{"a", "b"}
+	b := []string{"a"}
+
+	if slicecontains.SameStrings(a, b) {
+		t.Error("slices should not be equal")
+	}
+}
