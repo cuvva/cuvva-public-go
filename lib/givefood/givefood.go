b71475f430e9237733e967ca18ecce3de317af2a
James Billingham <billinghamj@users.noreply.github.com>
2021-02-08 18:03:19 +0000

    lib: clean up non-standard bits in `givefood` & `mixpanel` (#34)
    * givefood: fix inconsistent & non-standard field naming
    
    * mixpanel: fix variable naming and use context

--- lib/givefood/givefood.go
+++ lib/givefood/givefood.go
@@ -6,24 +6,24 @@ type FoodBank struct {
 	Address                   string     `json:"address"`
 	Postcode                  string     `json:"postcode"`
 	Country                   string     `json:"country"`
-	LatLng                    string     `json:"latt_long"`
+	LattLong                  string     `json:"latt_long"`
 	Closed                    bool       `json:"closed"`
 	Phone                     string     `json:"phone"`
 	Email                     string     `json:"email"`
-	Url                       string     `json:"url"`
-	ShoppingListUrl           string     `json:"shopping_list_url"`
+	URL                       string     `json:"url"`
+	ShoppingListURL           string     `json:"shopping_list_url"`
 	CharityNumber             string     `json:"charity_number"`
-	CharityRegisteredUrl      string     `json:"charity_register_url"`
+	CharityRegisteredURL      string     `json:"charity_register_url"`
 	Network                   string     `json:"network"`
 	ParliamentaryConstituency string     `json:"parliamentary_constituency"`
-	MpParty                   string     `json:"mp_party"`
-	Mp                        string     `json:"mp"`
+	MPParty                   string     `json:"mp_party"`
+	MP                        string     `json:"mp"`
 	District                  string     `json:"district"`
 	Ward                      string     `json:"ward"`
 	DistanceMi                float64    `json:"distance_mi"`
 	NumberNeeds               int        `json:"number_needs"`
 	Needs                     string     `json:"needs"`
-	NeedId                    string     `json:"need_id"`
+	NeedID                    string     `json:"need_id"`
 	Updated                   string     `json:"updated"`
 	UpdatedText               string     `json:"updated_text"`
 	Locations                 []Location `json:"locations"`
@@ -31,13 +31,13 @@ type FoodBank struct {
 
 type Location struct {
 	Phone                     string `json:"phone"`
-	LatLng                    string `json:"latt_long"`
+	LattLong                  string `json:"latt_long"`
 	ParliamentaryConstituency string `json:"parliamentary_constituency"`
 	Name                      string `json:"name"`
-	MpParty                   string `json:"mp_party"`
+	MPParty                   string `json:"mp_party"`
 	Address                   string `json:"address"`
 	District                  string `json:"district"`
 	Ward                      string `json:"ward"`
-	Mp                        string `json:"mp"`
+	MP                        string `json:"mp"`
 	Postcode                  string `json:"postcode"`
 }
