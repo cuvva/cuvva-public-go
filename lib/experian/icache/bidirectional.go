6e08c85e04fb02acadc6ac54794edcdd4e78afe4
James Billingham <git@jamesbillingham.com>
2021-02-09 17:03:30 +0000

    quite a lot more is actually bidi

--- lib/experian/icache/bidirectional.go
+++ lib/experian/icache/bidirectional.go
@@ -8,3 +8,63 @@ type Control struct {
 	ClientBranchNumber  *string
 	UserIdentity        *string
 }
+
+type ThirdPartyData struct {
+	XMLNS string `xml:"xmlns,attr"`
+
+	OptOut          Bool
+	TransientAssocs Bool
+	HHOAllowed      Bool
+}
+
+type Applicant struct {
+	XMLNS string `xml:"xmlns,attr"`
+
+	ApplicantIdentifier int
+
+	Name ApplicantName
+
+	DateOfBirth Date
+}
+
+type ApplicantName struct {
+	Forename   string
+	MiddleName *string
+	Surname    string
+}
+
+type LocationDetails struct {
+	XMLNS string `xml:"xmlns,attr"`
+
+	LocationIdentifier int
+
+	UKLocation LocationDetailsUKLocation
+}
+
+type LocationDetailsUKLocation struct {
+	Flat            *string
+	HouseName       *string
+	HouseNumber     *string
+	Street          *string
+	Street2         *string
+	District        *string
+	District2       *string
+	PostTown        *string
+	County          *string
+	Postcode        *string
+	POBox           *string
+	Country         *string
+	SharedLetterbox *string
+}
+
+type Residency struct {
+	XMLNS string `xml:"xmlns,attr"`
+
+	ApplicantIdentifier int
+	LocationIdentifier  int
+
+	LocationCode string
+
+	ResidencyDateFrom Date
+	ResidencyDateTo   Date
+}
