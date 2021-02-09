804a873a8822e7e0b20f74a1e9e79f58fc4ecfa3
James Billingham <git@jamesbillingham.com>
2021-02-09 18:32:09 +0000

    response types

---
+++ lib/experian/icache/response.go
@@ -0,0 +1,41 @@
+package icache
+
+import (
+	"encoding/xml"
+)
+
+type InteractiveResponse struct {
+	XMLName xml.Name `xml:"http://www.uk.experian.com/experian/wbsv/peinteractive/v100 InteractiveResponse"`
+
+	Root OutputRoot
+}
+
+type OutputRoot struct {
+	XMLName xml.Name `xml:"http://schemas.microsoft.com/BizTalk/2003/Any OutputRoot"`
+
+	Output Output
+}
+
+type Output struct {
+	XMLName xml.Name `xml:"http://schema.uk.experian.com/experian/cems/msgs/v1.1/ConsumerData Output"`
+
+	Control        Control
+	BureauMatchKey BureauMatchKey
+
+	Residencies []Residency `xml:"Residency"`
+
+	ConsumerSummary ConsumerSummary
+}
+
+type BureauMatchKey struct {
+	MatchCategory string
+}
+
+type ConsumerSummary struct {
+	Scoring  Map `xml:"PremiumValueData>Scoring"`
+	AddrLink Map `xml:"PremiumValueData>AddrLink"`
+	AgeDOB   Map `xml:"PremiumValueData>AgeDoB"`
+
+	ElectoralRoll Map `xml:"Summary>ElectoralRoll"`
+	PublicInfo    Map `xml:"Summary>PublicInfo"`
+}
