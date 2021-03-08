eac9586715ae5727499bb549f9631e1fc5166f53
Julian <jules.craske@cuvva.com>
2021-03-08 09:45:24 +0000

    jsonschema: add filesystem loader for use with embed (#74)

---
+++ lib/jsonschema/filesystem.go
@@ -0,0 +1,38 @@
+package jsonschema
+
+import (
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+
+	"github.com/xeipuuv/gojsonschema"
+)
+
+type FS struct {
+	raw fs.FS
+}
+
+// NewFS will typically be passed an embed.FS to easily load separate .json schema files
+func NewFS(raw fs.FS) *FS {
+	return &FS{raw: raw}
+}
+
+// Loads the given filename suffixing with ".json"
+func (f *FS) LoadJSONExt(filepath string) gojsonschema.JSONLoader {
+	return f.Load(fmt.Sprintf("%s.json", filepath))
+}
+
+// Loads the given filename
+func (f *FS) Load(filepath string) gojsonschema.JSONLoader {
+	file, err := f.raw.Open(filepath)
+	if err != nil {
+		panic(fmt.Errorf("open json schema file: %w", err))
+	}
+
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(fmt.Errorf("read json schema file: %s", err))
+	}
+
+	return gojsonschema.NewBytesLoader(b)
+}
