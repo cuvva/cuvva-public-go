db88b23c67c3dd63577106304dddfe33af662546
James Billingham <git@jamesbillingham.com>
2021-07-14 13:21:29 +0100

    improve

--- lib/jsonschema/ksuid_test.go
+++ lib/jsonschema/ksuid_test.go
@@ -7,7 +7,9 @@ import (
 )
 
 func TestKSUIDFormatChecker(t *testing.T) {
-	assert.Equal(t, false, ksuidFormatChecker{}.IsFormat("foo"))
-	assert.Equal(t, true, ksuidFormatChecker{}.IsFormat("user_000000CBEvdtGRrnrcQKCsSDNNKmR"))
-	assert.Equal(t, true, ksuidFormatChecker{}.IsFormat("test_user_000000CBEvefIcrYXsZObXFKZBQrh"))
+	c := ksuidFormatChecker{}
+
+	assert.Equal(t, false, c.IsFormat("foo"))
+	assert.Equal(t, true, c.IsFormat("user_000000CBEvdtGRrnrcQKCsSDNNKmR"))
+	assert.Equal(t, true, c.IsFormat("test_user_000000CBEvefIcrYXsZObXFKZBQrh"))
 }
