110e415214e8cf4493767ea3efcba8f2342f822a
Julian Craske <jules.craske@cuvva.com>
2021-07-22 15:49:54 +0100

    redis: allow configurable timeouts

--- lib/config/common.go
+++ lib/config/common.go
@@ -21,12 +21,24 @@ import (
 
 // Redis configures a connection to a Redis database.
 type Redis struct {
-	URI string `json:"uri"`
+	URI          string        `json:"uri"`
+	DialTimeout  time.Duration `json:"dial_timeout"`
+	ReadTimeout  time.Duration `json:"read_timeout"`
+	WriteTimeout time.Duration `json:"write_timeout"`
 }
 
 // Options returns a configured redis.Options structure.
 func (r Redis) Options() (*redis.Options, error) {
-	return redis.ParseURL(r.URI)
+	opts, err := redis.ParseURL(r.URI)
+	if err != nil {
+		return nil, err
+	}
+
+	opts.DialTimeout = r.DialTimeout
+	opts.ReadTimeout = r.ReadTimeout
+	opts.WriteTimeout = r.WriteTimeout
+
+	return opts, nil
 }
 
 // Connect returns a connected redis.Client instance.
