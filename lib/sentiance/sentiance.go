5032f3e229d646a11356cf56efddc3a07a4f9340
Patrick Socha <git@psocha.co.uk>
2021-08-24 16:44:21 +0300

    sentiance lib: return trips and analysis type for event via graphql (#245)
    * sentiance lib: return trips and analysis_type for event via graphql

--- lib/sentiance/sentiance.go
+++ lib/sentiance/sentiance.go
@@ -27,6 +27,11 @@ type GetEventByIDRequest struct {
 	EventID         string `json:"event_id"`
 }
 
+type GetEventAndWaypointsByIDRequest struct {
+	SentianceUserID string `json:"sentiance_user_id"`
+	EventID         string `json:"event_id"`
+}
+
 type EnrichTransportEventResponse struct {
 	EventID      string `json:"event_id"`
 	Type         string `json:"type"`
@@ -53,4 +58,39 @@ type UserResponse struct {
 	ID           string                          `json:"id"`
 	ExternalID   string                          `json:"external_id"`
 	EventHistory []*EnrichTransportEventResponse `json:"event_history"`
+}
+
+type TransportEventWithWaypointsResponse struct {
+	Data TransportEventWithWaypointsData `json:"data"`
+}
+
+type TransportEventWithWaypointsData struct {
+	User UserWithWaypoints `json:"user"`
+}
+
+type UserWithWaypoints struct {
+	ID           string             `json:"id"`
+	ExternalID   string             `json:"external_id"`
+	EventHistory []*TransportEvents `json:"event_history"`
+}
+
+type TransportEvents struct {
+	EventID      string     `json:"event_id"`
+	Type         string     `json:"type"`
+	Mode         string     `json:"mode"`
+	OccupantRole string     `json:"occupant_role"`
+	AnalysisType string     `json:"analysis_type"`
+	Start        string     `json:"start"`
+	End          string     `json:"end"`
+	Waypoints    []Waypoint `json:"waypoints"`
+}
+
+type Waypoint struct {
+	Type      string    `json:"type"`
+	Latitude  int64     `json:"longitude"`
+	Longitude int64     `json:"latitude"`
+	Timestamp time.Time `json:"timestamp"`
+	Accuracy  int       `json:"accuracy"`
+	Speed     int64     `json:"speed"`
+	Altitude  int64     `json:"altitude"`
 }
