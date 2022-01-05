924ab6e7a68897702929ae2ff2fab42d4596e706
Andy Provan <git@provan.uk>
2022-01-05 15:26:58 +0000

    slicecontains: add Elem and SameElems functions

--- lib/slicecontains/slicecontains.go
+++ lib/slicecontains/slicecontains.go
@@ -1,6 +1,7 @@
 package slicecontains
 
-func String(slice []string, val string) bool {
+// Elem checks if an element is contained within a slice
+func Elem[E comparable](slice []E, val E) bool {
 	for _, v := range slice {
 		if v == val {
 			return true
@@ -10,13 +11,13 @@ func String(slice []string, val string) bool {
 	return false
 }
 
-// SameStrings checks that all of the strings exist in a and b.
-func SameStrings(a, b []string) bool {
+// SameElems checks that all the elements exist in both a and b.
+func SameElems[E comparable](a, b []E) bool {
 	if len(a) != len(b) {
 		return false
 	}
 
-	keys := map[string]struct{}{}
+	keys := map[E]struct{}{}
 	for _, v := range a {
 		keys[v] = struct{}{}
 	}
@@ -28,4 +29,15 @@ func SameStrings(a, b []string) bool {
 	}
 
 	return true
+}
+
+// Deprecated: Use Elem instead
+func String(slice []string, val string) bool {
+	return Elem(slice, val)
+}
+
+// Deprecated: Use SameElems instead
+// SameStrings checks that all of the strings exist in a and b.
+func SameStrings(a, b []string) bool {
+	return SameElems(a,b)
 }
--- lib/slicecontains/slicescontains_test.go
+++ lib/slicecontains/slicescontains_test.go
@@ -39,3 +39,51 @@ func TestSameStringsSubset(t *testing.T) {
 		t.Error("slices should not be equal")
 	}
 }
+
+func TestElem(t *testing.T) {
+	a := []string{"a", "b"}
+	b := "b"
+	c := "c"
+
+	if !slicecontains.Elem(a, b) {
+		t.Error("b is contained within a")
+	}
+
+	if slicecontains.Elem(a, c) {
+		t.Error("c is not contained within a")
+	}
+}
+
+func TestElemsEmpty(t *testing.T) {
+	var a []int
+	var b []int
+
+	if !slicecontains.SameElems(a, b) {
+		t.Error("slices should be equal")
+	}
+
+	a = []int{}
+	b = []int{}
+
+	if !slicecontains.SameElems(a, b) {
+		t.Error("slices should be equal")
+	}
+}
+
+func TestSameElemsMatch(t *testing.T) {
+	a := []int{0, 1}
+	b := []int{0, 2}
+
+	if slicecontains.SameElems(a, b) {
+		t.Error("slices should not be equal")
+	}
+}
+
+func TestSameElemsStringsSubset(t *testing.T) {
+	a := []int{0, 1}
+	b := []int{0}
+
+	if slicecontains.SameElems(a, b) {
+		t.Error("slices should not be equal")
+	}
+}
