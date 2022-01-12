102a205b7ff1bb3b0c65191e57733bac11bfcddc
George Wilson <github@g-wilson.co.uk>
2022-01-12 17:27:08 +0000

    remove cmt (#337)

--- lib/cmt/cmt.go
+++
@@ -1,9 +0,0 @@
-package cmt
-
-type AuthorizationCodeRequest struct {
-	AccountID string `json:"account_id"`
-}
-
-type AuthorizationCodeResponse struct {
-	AuthorizationCode string `json:"auth_code"`
-}
