71d06214e1710807b0dd272f09379c36bb33f4eb
James Billingham <git@jamesbillingham.com>
2022-01-30 09:36:30 +0000

    add context-based use case

--- lib/servicecontext/servicecontext.go
+++ lib/servicecontext/servicecontext.go
@@ -1,5 +1,9 @@
 package servicecontext
 
+import (
+	"context"
+)
+
 // Info type holds useful info about the currently-running service
 type Info struct {
 	Name        string
@@ -28,4 +32,27 @@ func Get() Info {
 // IsSet returns true if the singleton has been initialised
 func IsSet() bool {
 	return service != nil
+}
+
+type contextKey string
+
+var (
+	infoContextKey = contextKey("info")
+)
+
+// SetContext wraps the context with the service info
+func SetContext(ctx context.Context, name, env string) context.Context {
+	return context.WithValue(ctx, infoContextKey, &Info{
+		Name:        name,
+		Environment: env,
+	})
+}
+
+// GetContext retrieves the service info from the context
+func GetContext(ctx context.Context) *Info {
+	if val, ok := ctx.Value(infoContextKey).(*Info); ok {
+		return val
+	}
+
+	return nil
 }
