4c4143e7dbf9aa682e572861473af04fa79db524
Mario Jorge Silveira Pereira <mario.pereira@cuvva.com>
2023-11-16 12:11:26 +0000

    context: added panic recovery to forked contexts

--- lib/middleware/request/context.go
+++ lib/middleware/request/context.go
@@ -2,6 +2,7 @@ package request
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/cuvva/cuvva-public-go/lib/clog"
@@ -39,6 +40,14 @@ type ContextKey string
 
 // ForkContext provides a callback function with a new context inheriting values from the request context, and will log any error returned by the callback
 func ForkContext(ctx context.Context, fn func(context.Context) error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err := fmt.Errorf("recovered from panic: %v", r)
+			if err != nil {
+				panic(r)
+			}
+		}
+	}()
 	newCtx := cloneContext(ctx)
 
 	go func() {
@@ -51,6 +60,15 @@ func ForkContext(ctx context.Context, fn func(context.Context) error) {
 
 // ForkContextWithTimeout provides a callback function with a new context inheriting values from the request context with a timeout, and will log any error returned by the callback
 func ForkContextWithTimeout(ctx context.Context, timeout time.Duration, fn func(context.Context) error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err := fmt.Errorf("recovered from panic: %v", r)
+			if err != nil {
+				panic(r)
+			}
+		}
+	}()
+
 	newCtx, cancel := context.WithTimeout(cloneContext(ctx), timeout)
 
 	go func() {
