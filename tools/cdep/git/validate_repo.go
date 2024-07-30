82f56d4b0cb98a23b46c90c2139b5ea9683d46e8
Lawrence-Dawson <24669349+Lawrence-Dawson@users.noreply.github.com>
2024-07-30 16:32:30 +0100

    cdep: add check for remote repo ssh url (#778)
    * cdep: add check for remote repo ssh url

--- tools/cdep/git/validate_repo.go
+++ tools/cdep/git/validate_repo.go
@@ -1,6 +1,9 @@
 package git
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/cuvva/cuvva-public-go/lib/cher"
 	gogit "github.com/go-git/go-git/v5"
 )
@@ -15,5 +18,21 @@ func CheckRepo(repo *gogit.Repository) (*gogit.Remote, error) {
 		return nil, cher.New("multiple_remotes", nil)
 	}
 
+	if len(remotes) == 0 {
+		return nil, cher.New("no_remotes", nil)
+	}
+
+	remote := remotes[0]
+
+	if !isRemoteURLSSH(remote) {
+		return nil, errors.New("cuvva repo remote origin url is not ssh")
+	}
+
 	return remotes[0], nil
+}
+
+func isRemoteURLSSH(remote *gogit.Remote) bool {
+	remoteURL := remote.Config().URLs[0]
+
+	return strings.Contains(remoteURL, "git@")
 }
